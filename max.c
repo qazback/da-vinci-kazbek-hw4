6bd4810b19bba567ca33b09724435e5b185eceed
qazback <124498311+qazback@users.noreply.github.com>
2023-04-08 15:13:26 +0600

    feat: added a 1 task of hw4

---
+++ max.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int max(int array[], int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number of elements: ");
+    }
+    while (n < 1);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = get_int("Element %i: ", i);
+    }
+
+    printf("The max value is %i.\n", max(arr, n));
+}
+
+int max(int array[], int n)
+{
+    int max_val = array[0];
+
+    for (int i = 1; i < n; i++) {
+        if (array[i] > max_val) {
+            max_val = array[i];
+        }
+    }
+
+    return max_val; 
+}
